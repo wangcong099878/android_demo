3281854119750edc30d25f1a2fa55cf64fcd74e6
theoneee <625805189@qq.com>
2019-08-07 16:55:36 +0800

    更新Demo.apk

--- app/src/main/java/the/one/demo/App.java
+++ app/src/main/java/the/one/demo/App.java
@@ -37,7 +37,7 @@ public class App extends BaseApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-//        initFilePath();
+        initFilePath();
         initStatusDrawable();
 
         if (!LeakCanary.isInAnalyzerProcess(this)) {
